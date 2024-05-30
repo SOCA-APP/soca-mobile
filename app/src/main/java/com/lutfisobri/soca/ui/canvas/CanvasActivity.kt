24a656fbc8f5da5fbc9321c8d32aea774f02d6fb
Bulan Jatuh <lutfisobri@gmail.com>
2024-05-31 00:10:30 +0700

    slicing: add base file

---
+++ app/src/main/java/com/lutfisobri/soca/ui/canvas/CanvasActivity.kt
@@ -0,0 +1,21 @@
+package com.lutfisobri.soca.ui.canvas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.lutfisobri.soca.R
+
+class CanvasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_canvas)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
