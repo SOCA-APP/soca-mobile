be8ca8239e406a7ee00838ec280bf04abfef990a
Bulan Jatuh <lutfisobri@gmail.com>
2024-05-31 00:02:15 +0700

    Initial commit

---
+++ app/src/main/java/com/lutfisobri/soca/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.lutfisobri.soca.ui.splash
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.lutfisobri.soca.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
